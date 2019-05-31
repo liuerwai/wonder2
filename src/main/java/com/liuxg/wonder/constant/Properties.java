6ab5892e7f9055d666f1d99f712c05c74eff8261
liuxg <xxx@xx.com>
2019-05-31 22:54:33 +0800

    修改代码

--- src/main/java/com/liuxg/wonder/constant/Properties.java
+++ src/main/java/com/liuxg/wonder/constant/Properties.java
@@ -1,10 +1,10 @@
 package com.liuxg.wonder.constant;
 
 public class Properties {
-//    public static final String PackagePath = "C:\\wonder\\";
-//    public static final String StaticPath = "C:\\wonder\\static\\";
-    public static final String PackagePath = "D:\\code\\wonder2\\src\\main\\resources\\";
-    public static final String StaticPath = "D:\\code\\wonder2\\src\\main\\resources\\static\\";
+    public static final String PackagePath = "C:\\wonder\\";
+    public static final String StaticPath = "C:\\wonder\\static\\";
+//    public static final String PackagePath = "D:\\code\\wonder2\\src\\main\\resources\\";
+//    public static final String StaticPath = "D:\\code\\wonder2\\src\\main\\resources\\static\\";
     public static final String ImagePath = StaticPath + "images\\";
     public static final String HomeImage = "home";
     public static final String IndexVideio = "test";
